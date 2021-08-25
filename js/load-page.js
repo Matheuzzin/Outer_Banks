afdb6c84f300658260f454d6eefb9a5560113029
Matheus Antônio <mm0389175@gmail.com>
2021-08-24 22:03:21 -0300

    commit atualizações

--- js/load-page.js
+++ js/load-page.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 // Monitorar todos os cliques em cima do elemento "a" do nosso documento HTML
 
 $('a').click(function(e){
-    e.preventDefault()
+    e.preventDefault() //Anula a funcionalidade do HTML de abrir o link
 
-    //Criar uma variável local que receba o atributo href do link
+    //Criar uma variável local(só funciona naquele local)-(global funciona no documento inteiro) que receba o atributo href do link
 
     let page = $(this).attr('href')
 
@@ -15,47 +15,40 @@ $('a').click(function(e){
 
     //Verificar qual conteúdo eu devo carregar
     switch(page){
-        //Se for para abrir a página sobre mim...
-        case 'about': 
-            $('.modal-title').append('Sobre mim')
+        //Se for para abrir a página Sinópse...
+        case 'sinopse': 
+            $('.modal-title').append('Sinópse')
             $('.modal-body').append(`<p>
-            Meu nome é Matheus Antônio, tenho 15 anos e moro na cidade de Lins-SP.
-            </p>`)
+                Criada por Justin Roiland e Dan Harmon, Rick & Morty é uma série animada de comédia que mostra as aventuras interdimensionais de Rick, a mente mais brilhante da galáxia, e seu neto Morty. <br>
+            </p>
+            <p>
+                Com sua arma capaz de criar portais para viajar no tempo-espaço, Rick leva Morty para explorar todos as absurdas formas de vida que o universo é capaz de sustentar. <br>
+            </p>
+            <p>
+                Apesar de genial, Rick tem sofre de alcoolismo e possui uma relação conflituosa com sua família, fruto da sua visão cínica da vida e seu egoísmo. Por outro lado, Morty está longe de ter o intelecto do seu avô, mas é o único capaz de fazê-lo ser um pouquinho mais gentil.<br>
+            </p>
+            <p>
+                Entre momentos hilários e reflexões interessantes sobre a vida, Rick & Morty arranca gargalhadas e ainda rende ótimas discussões filo`)
             $('#modal-info').modal('show')
         break
 
-        //Se for para abrir a página Formação...
-        case 'formacao': 
-            $('.modal-title').append('Minha Formação')
-            $('.modal-body').append(`<p>
-            <b>Formação Acadêmica:</b> <br><br>
-            Cursando o 2° ano do Ensino Médio em Desenvolvimento de Sistemas.
-            </p>`)
+        //Se for para abrir a página Temporadas...
+        case 'temporadas': 
+            $('.modal-title').append('Temporadas')
+            $('.modal-body').append(`<p>O seriado de Rick and morty contém até o momento 5 temporadas</p>`)
             $('#modal-info').modal('show')
         break
 
-        //Se for para abrir a página Trabalhos...
-        case 'trabalhos': 
-            $('.modal-title').append('Trabalhos')
-            $('.modal-body').append(`<p> <b>Experiência profissional</b> <br><br>
-            Mecânico de manutenção de máquinas em geral <br>
-            Lins Agroindustrial S.A.- Lins-SP <br>
-            Mar 2020 – Dez 2020
-            </p>`)
+        //Se for para abrir a página Curiosidades...
+        case 'curiosidades': 
+            $('.modal-title').append('Curiosidades')
+            $('.modal-body').append(`<p> 1- O Seriado foi criado, vendido e roteirizado em apenas um dia</p> <br>
+            <p>2- Originalmente, Rick e Morty eram paródias de Doc Brown e Marty McFly </p> <br>
+            <p>3- A série foi uma das primeiras a estrear um episódio no Instagram </p> <br>
+            <p>4- Todos os arrotos de Rick são reais</p>`)
             $('#modal-info').modal('show')
         break
 
-        //Se for para abrir a página Contatos...
-        case 'contatos': 
-            $('.modal-title').append('Contatos')
-            $('.modal-body').append(`<p>
-            <b>Telefone:</b> <br><br>
-            +55 14 99769-2498
-            </p>`)
-            $('#modal-info').modal('show')
-        break
-
-        default: alert('Link não encontrado');
     }
 
     })
